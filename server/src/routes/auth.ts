56a3346f2e4fe920b78da5c18a8709886f96957a
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-05-18 20:42:31 -0300

    Finalizada a construção das rotas autenticadas

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import axios from 'axios'
 import { z } from 'zod'
+import { prisma } from '../lib/prisma'
 
 export async function authRoutes(app:FastifyInstance) {
   app.post('/register', async(request) => {
@@ -33,10 +34,43 @@ export async function authRoutes(app:FastifyInstance) {
       },
     })
 
-    const user = userResponse.data
+    const userSchema = z.object({
+      id: z.number(),
+      login: z.string(),
+      name: z.string(),
+      avatar_url: z.string().url(),
+    })
+
+    const userInfo = userSchema.parse(userResponse.data)
+
+    let user = await prisma.user.findUnique({
+      where: {
+        githubID: userInfo.id,
+      }
+    })
+
+    if (!user) {
+      user = await prisma.user.create({
+        data: {
+          githubID: userInfo.id,
+          login: userInfo.login,
+          name: userInfo.name,
+          avatarUrl: userInfo.avatar_url,
+        },
+      })
+    }
+
+    const token = app.jwt.sign({
+      name: user.name,
+      avatarUrl: user.avatarUrl,
+    }, {
+      sub: user.id,
+      expiresIn: '30 days',
+    },
+    )
 
     return {
-      user,
+      token,
     }
   })
 }
