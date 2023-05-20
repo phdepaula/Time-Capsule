32419031912cba32fe68d7675a10f1a8ff3025e8
phdepaula <pedrohenriquedepaulaalves@yahoo.com.br>
2023-05-20 11:04:52 -0300

    Configurando layout, memória e middleware da web

--- web/src/components/Profile.tsx
+++ web/src/components/Profile.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import { getUser } from '../lib/auth'
 import Image from 'next/image'
 
@@ -16,7 +17,10 @@ export function Profile() {
 
       <p className="max-w-[200px] text-sm leading-snug">
         {name}
-        <a href="" className="block text-red-400 hover:text-red-300">
+        <a
+          href="/api/auth/logout"
+          className="block text-red-400 hover:text-red-300"
+        >
           Quero sair
         </a>
       </p>
